948f3299e60a0fe6a519b06c3b5e0406b7637a16
Bqnic <123201175+Bqnic@users.noreply.github.com>
2024-03-24 15:29:43 +0100

    final commit, only student registration and login was completed

--- BackGet/obj/Debug/net8.0/BackGet.AssemblyInfo.cs
+++ BackGet/obj/Debug/net8.0/BackGet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackGet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ea545a014345a71d57eaff044a77c8e782043e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a963e0e6a186410feacca83e380cc548d984ef51")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackGet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackGet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
